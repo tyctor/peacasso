4f4c0d9abae5b4d5d33d2b434e17ae5f4e5e8551
Viktor <viktor.vraniak@gmail.com>
2022-10-25 16:59:59 +0200

    Removed ui command from peacassomh
    only ws command is needed in current distributed ecosystem

--- peacasso/climh.py
+++ peacasso/climh.py
@@ -10,22 +10,6 @@ app = typer.Typer()
 
 
 @app.command()
-def ui(
-    host: str = "127.0.0.1", port: int = 8081, workers: int = 1, reload: bool = True
-):
-    """
-    Launch the peacasso UI.Pass in parameters host, port, workers, and reload to override the default values.
-    """
-    uvicorn.run(
-        "peacasso.web.backend.appmh:app",
-        host=host,
-        port=port,
-        workers=workers,
-        reload=reload,
-    )
-
-
-@app.command()
 def ws(
     scheme: str = "ws",
     host: str = "meaningful.noir.studio",
